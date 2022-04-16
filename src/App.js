ac10a1ddc9222fecb373ebfb062517c213ef7363
joceilton <joceilton.g@gmail.com>
2022-04-16 08:07:31 -0300

    Update App.js cccccccccc

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import "./styles.css";
-import {
-BrowserRouter as Router,
-Route,
-Switch,
-Link
-} from "react-router-dom"
+import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+//import Home from "./components/Pages/Home"
 function Home() {
-return (
-<div> Home </div>
-)
+  return <div> Home </div>;
+}
+
+function Arq() {
+  return <>ownTAN4UubERC-9_shPBCeATNTr1k669ieR1</>;
 }
 
 export default function App() {
   return (
     <div className="App">
       <Router>
-         <Switch>
-           <Route path="/">
-              <Home/>
-           </Route>
-         </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/ownTAN4UubERC-9_shPBCeATNTr1k669ieR1.html"
+            element={<Arq />}
+          />
+        </Routes>
       </Router>
     </div>
   );
